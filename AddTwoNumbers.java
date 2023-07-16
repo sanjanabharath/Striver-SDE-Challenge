b66adabbd07236994e74b8e86cae1f70dfdc73d1
sanjanabharath <109329602+sanjanabharath@users.noreply.github.com>
2023-07-16 20:15:10 +0530

    Update AddTwoNumbers.java

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,4 +1,4 @@
-//https://leetcode.com/problems/add-two-numbers/submissions/
+//https://leetcode.com/problems/add-two-numbers/description/
 
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
