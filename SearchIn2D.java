b68091b81f2cdd6f0b8f6de3301bdd589820c588
sanjanabharath <109329602+sanjanabharath@users.noreply.github.com>
2023-07-16 20:14:46 +0530

    Update SearchIn2D.java

--- SearchIn2D.java
+++ SearchIn2D.java
@@ -1,3 +1,5 @@
+//https://leetcode.com/problems/search-a-2d-matrix/description/
+
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int i = 0, j = matrix[0].length-1;
