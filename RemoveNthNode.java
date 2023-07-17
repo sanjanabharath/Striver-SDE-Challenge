d49a0cb754c92e975b1cbdc499d3593c726ad991
sanjanabharath <109329602+sanjanabharath@users.noreply.github.com>
2023-07-17 20:23:21 +0530

    Create RemoveNthNode.java

---
+++ RemoveNthNode.java
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode temp = new ListNode(0);
+        temp.next = head;
+
+        ListNode slow = temp;
+        ListNode fast = temp;
+
+        for(int i=1; i<=n+1; i++){
+            fast = fast.next;
+        }
+
+        while(fast!=null){
+            slow=slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return temp.next;
+    }
+}
